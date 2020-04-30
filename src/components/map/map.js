30824dd3ab220624fff96e4096896cbe3e1d03a5
Apelsin16 <apelsin@i.ua>
2020-04-30 11:08:27 +0300

    first render is well, marker temporary is well

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -4,29 +4,31 @@ import { API } from '../../constants';
 import home from './location.svg';
 
 const markersArr = [
-    {type: 'puppy', lan: 50.47, lng: 30.50},
-    {type: 'nursery', lan: 50.48, lng: 30.61},
-    {type: 'help', lan: 50.46, lng: 30.52},
-    {type: 'help', lan: 50.45, lng: 30.63},
-    {type: 'shop', lan: 50.49, lng: 30.44},
-    {type: 'veterinary', lan: 50.44, lng: 30.55},
-    {type: 'puppy', lan: 55.6, lng: 37.7},
-    {type: 'nursery', lan: 55.7, lng: 37.5},
-    {type: 'help', lan: 55.8, lng: 37.8},
+    {type: 'P', lan: 50.47, lng: 30.50},
+    {type: 'N', lan: 50.48, lng: 30.61},
+    {type: 'H', lan: 50.46, lng: 30.52},
+    {type: 'H', lan: 50.45, lng: 30.63},
+    {type: 'S', lan: 50.49, lng: 30.44},
+    {type: 'V', lan: 50.44, lng: 30.55},
+    {type: 'P', lan: 55.6, lng: 37.7},
+    {type: 'N', lan: 55.7, lng: 37.5},
+    {type: 'H', lan: 55.8, lng: 37.8},
 ]
 
 const Marker = ({ text, img }) => (
     <div style={{
-        // color: 'white',
-        // background: 'pink',
-        // padding: '15px 10px',
+        color: '#483D8B',
+        background: 'rgba(0, 0, 0, 0.3)',
         width: '35px',
+        height: '35px',
+        padding: '5px',
         display: 'inline-flex',
-        // textAlign: 'center',
-        // alignItems: 'center',
-        // justifyContent: 'center',
-        // borderRadius: '100%',
-        transform: 'translate(-50%, -50%)'
+        textAlign: 'center',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: '100%',
+        transform: 'translate(-50%, -50%)',
+        fontSize: '20px'
 }}>
 {text ? text : <img src={img} alt="Marker"/>}
 </div>
@@ -76,7 +78,7 @@ export default class ReactMap extends Component {
                             />
                         )
                     })}
-                    <Marker lat={this.state.center.lat} img={home} lng={this.state.center.lng} />
+                    <Marker lat={this.state.center.lat} lng={this.state.center.lng} text="&#9971;" />
                 </GoodleMapReact>
             </div>
         )
